5fdc5555ae19cf42de4cab5315d37d8cc0a023f8
SkogisREAL <filip.skoglund07@outlook.com>
2024-02-11 22:06:48 +0100

    changed to .env

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const express = require('express');
-const config = require('./config.json');
 const cors = require('cors');
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences] });
+require('dotenv').config();
+
+const token = process.env.TOKEN;
+const guildId = process.env.GUILD_ID;
+const userId = process.env.USER_ID;
+const port = process.env.PORT;
 
 client.on('ready', () => {
     console.log(`Bot is ready as: ${client.user.tag}`);
 });
 
-client.login(config.token);
+client.login(token);
 
 const app = express();
 app.use(cors());
 
 app.get('/api/status', async (req, res) => {
     try {
-        const guild = client.guilds.cache.get(config.guildId);
-        const member = await guild.members.fetch(config.userId);
+        const guild = client.guilds.cache.get(guildId);
+        const member = await guild.members.fetch(userId);
         if (member && member.presence) {
             if (member.presence.status === 'dnd') {
                 res.json({ status: 'do not disturb' });
@@ -32,4 +37,4 @@ app.get('/api/status', async (req, res) => {
     }
 });
 
-app.listen(config.port, () => console.log(`Status-server running on port ${config.port}`));
+app.listen(port, () => console.log(`Status-server running on port ${port}`));
